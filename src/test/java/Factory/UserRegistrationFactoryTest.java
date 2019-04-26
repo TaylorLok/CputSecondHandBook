6464006f31985c91de87dd60e6b62eb23b72e571
TaylorLok <lokombetaylor1223@gmail.com>
2019-04-26 23:28:35 +0200

    in progress

--- src/test/java/Factory/UserRegistrationFactoryTest.java
+++ src/test/java/Factory/UserRegistrationFactoryTest.java
@@ -1,22 +1,38 @@
 package Factory;
 
+import Domain.UserRegistration;
 import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class UserRegistrationFactoryTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    UserRegistration userReg;
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
+        userReg = new UserRegistration.Builder()
+                                      .build();
     }
 
     @Test
-    public void getUserRegistration() {
+    public void getUserRegistration()
+    {
+        userReg = new UserRegistration.Builder()
+                                      .name("Bintu")
+                                      .surname("Ngoune")
+                                      .studentNumber("214291324")
+                                      .emailAddress("tabaro@cput.ac.za")
+                                      .password("bintu002")
+                                      .build();
+
+
+        Assert.assertNotNull(userReg);
+        Assert.assertEquals("Bintu",userReg.getName());
+        Assert.assertEquals("Ngoune",userReg.getSurname());
+        Assert.assertEquals("214291324",userReg.getStudentNumber());
+        Assert.assertEquals("tabaro@cput.ac.za",userReg.getEmailAddress());
+        Assert.assertEquals("bintu002",userReg.getPassword());
     }
 }
