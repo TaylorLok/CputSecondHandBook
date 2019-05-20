2e2e88a527726ca1f539d63dcf4c5f325b3348d8
TaylorLok <lokombetaylor1223@gmail.com>
2019-05-20 06:40:06 +0200

    controller and test

--- src/main/java/Controller/IndexController.java
+++ src/main/java/Controller/IndexController.java
@@ -1,9 +1,18 @@
 package Controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IndexController
 {
+    @GetMapping("/")
+    @ResponseBody
     public String getHome()
     {
+
         return "Hello World";
     }
 }
---
+++ src/test/java/Controller/AdminControllerTest.java
@@ -0,0 +1,72 @@
+package Controller;
+
+import Domain.Admin;
+import Factory.AdminFactory;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+
+public class AdminControllerTest
+{
+   @Autowired
+   private TestRestTemplate restTemplate;
+   private String baseURL = "http://localhost:8080/Admin";
+
+
+
+
+
+    @Test
+    public void getAll()
+    {
+        HttpHeaders headers = new HttpHeaders();
+
+        HttpEntity<String> entity = new HttpEntity<String>(null,headers);
+        ResponseEntity<String> response = restTemplate.exchange(baseURL + "/read/all", HttpMethod.GET,entity,String.class);
+        assertNotNull(response.getBody());
+    }
+
+    @Ignore
+    public void testGetAdmin()
+    {
+        Admin admin = restTemplate.getForObject(baseURL + "/Admin/permis",Admin.class);
+        System.out.println(admin.getUser());
+        System.out.println(admin.getPermission());
+        System.out.println(admin.manageAdvert());
+        System.out.println(admin.manageBook());
+        assertNotNull(admin);
+    }
+
+    @Test
+    public void create()
+    {
+       Admin admin = AdminFactory.getAdmin("allowed");
+
+       ResponseEntity<Admin> posResponse = restTemplate.postForEntity(baseURL + "/create",admin,Admin.class);
+       assertNotNull(posResponse);
+       assertNotNull(posResponse.getBody());
+    }
+
+    @Test
+    public void read() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+
+}
